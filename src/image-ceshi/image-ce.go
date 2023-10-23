56709e6bf970b3896b55c323a6e1dab72104347b
tongWz <tongweizhen@foxmail.com>
2023-10-23 20:57:45 +0800

    xiugai

--- src/image-ceshi/image-ce.go
+++ src/image-ceshi/image-ce.go
@@ -19,17 +19,18 @@ func Image(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	err = html.Execute(writer, nil)
+
 	check(err)
 }
 
 func main() {
 	http.HandleFunc("/", Image)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("image"))))
 	dir, err := os.Getwd()
 	if err != nil {
 		fmt.Println("获取当前工作目录失败：", err)
 	} else {
 		fmt.Println("当前工作目录：", dir)
 	}
-	http.Handle("/image", http.StripPrefix("/image", http.FileServer(http.Dir("./image"))))
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
