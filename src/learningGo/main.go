1fb991795e641d5f3350e3894ad34fc222bc4253
tongwz <tongweizhen@foxmail.com>
2025-01-15 16:47:41 +0800

    xiugia

--- src/learningGo/main.go
+++ src/learningGo/main.go
@@ -11,14 +11,45 @@ func main() {
 	// 加入日志
 	apiServer.Use(gin.Logger())
 	// apiServer.Use(handle.GinLogHandle())
+	/**
+	假装代码
+	假装代码
+	假装代码
+	假装代码
+	假装代码
+	假装代码
+	// 注释2
+	// 注释2
+	// 注释2
+	// 注释2
+	// 注释2
+	// 注释2
+	// 注释2
+	// 注释2
+	注释2
+	注释2
+	注释2
+	注释2
+	*/
 
 	imageGroup := apiServer.Group("/image")
 
 	imageGroup.GET("/:name", controller.ImageShow)
 	// 路由声明
-	// routers.NewApis(r.Group(""))
+	// routers.NewApiSss(r.Group(""))
 	// routers.NewPCApi(r.Group(""))
 
 	// 设置api端口
 	_ = apiServer.Run(":8099")
+	// 注释
+	// 注释
+	// 注释
+	// 注释
+	// 注释
+	// 注释
+	// 注释
+	// 注释
+	// 注释
+	// 注释
+	// 注释
 }
